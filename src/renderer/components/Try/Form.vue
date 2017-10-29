579f6f7e1ac137369a411a1da500976f3a0a48d1
songlairui <cnre@live.cn>
2017-10-29 22:30:03 +0800

    测试filereader

--- src/renderer/components/Try/Form.vue
+++ src/renderer/components/Try/Form.vue
@@ -6,6 +6,8 @@
     br
     h1 {{ value }}
     h2 {{ filePath }}
+    br
+    blockquote MDN参考链接：https://developer.mozilla.org/zh-CN/docs/Web/API/FileReader
 </template>
 <script>
 export default {
@@ -20,6 +22,17 @@ export default {
       let file = (e.target.files || e.dataTransfer.files)[0]
       console.info({ file })
       this.filePath = file && file['path']
+      console.group('fileType')
+      console.log(file.type.match(/image\/*/) ? "is image" : 'is not image')
+      console.groupEnd()
+      let reader = new FileReader()
+      reader.onload = (e) => {
+        console.info('file loaded')
+        let ab = e.target.result
+        console.info(Buffer.from(ab))
+      }
+      reader.readAsArrayBuffer(file)
+
     },
     debug(...x) {
       console.info('params ', x)
